75fd50d59372b5ce7b556377ee5977866210ef15
philsmart <philsmart@v3059-0a9004c5.wifi.cf.ac.uk>
2017-01-10 16:20:35 +0000

    first checkin

---
+++ src/test/java/uk/ac/cardiff/raptor/harvest/RaptorHarvestApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.cardiff.raptor.harvest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaptorHarvestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
