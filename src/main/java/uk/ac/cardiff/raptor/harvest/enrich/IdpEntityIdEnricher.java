212daa8bbbaec0ba343f45a5d40a271ae5032f12
phil <smartp@cf.ac.uk>
2018-01-09 16:19:34 +0000

    fixed merge issues

--- src/main/java/uk/ac/cardiff/raptor/harvest/enrich/IdpEntityIdEnricher.java
+++ src/main/java/uk/ac/cardiff/raptor/harvest/enrich/IdpEntityIdEnricher.java
@@ -16,18 +16,12 @@ import uk.ac.cardiff.model.event.Event;
 import uk.ac.cardiff.model.event.ShibbolethIdpAuthenticationEvent;
 
 /**
-<<<<<<< HEAD
- * Adds the idpEntityID to any
- * {@link ShibbolethIdpAuthenticationEvent#setServiceId(String)} from the input
- * {@link List} of {@link Event}s. Can be null, hence will not set. Only sets
- * the serviceId if one does not already exist.
  * 
-=======
  * Adds the idpEntityID to any {@link ShibbolethIdpAuthenticationEvent} from the
- * input {@link List} of {@link Event}s. Can be null, hence will not set. Only
- * sets the values if one does not already exist.
+ * input {@link List} of {@link Event}s. Can be not be null, hence will not set.
+ * Only sets the values if one does not already exist.
  *
->>>>>>> 30b5806520f1ecbdd733222c390b9a2c310ce675
+ * 
  * @author philsmart
  *
  */
@@ -40,7 +34,6 @@ public class IdpEntityIdEnricher implements AttributeEnrichment {
 	private static final Logger log = LoggerFactory.getLogger(IdpEntityIdEnricher.class);
 
 	/**
-<<<<<<< HEAD
 	 * The entityID of the {@link Event}s originating Identity Provider. Can not be
 	 * null. Will not overwrite an existing value. Only applies to
 	 * {@link ShibbolethIdpAuthenticationEvent}s for the Shibboleth V2 parser.
@@ -54,25 +47,15 @@ public class IdpEntityIdEnricher implements AttributeEnrichment {
 		log.info("Auto-creating Shibboleth v2.x EntityID as serviceID enricher");
 	}
 
-=======
-	 * The entityID of the {@link Event}s originating Identity Provider. Can be
-	 * null for no-op functionality. Will not overwrite an existing value. Only
-	 * applies to {@link ShibbolethIdpAuthenticationEvent}s for the Shibboleth
-	 * V2 parser.
-	 */
-	private String idpEntityId;
-
 	/**
 	 * For each {@link Event} in {@code events}, enriches it iff it is a
-	 * {@link ShibbolethIdpAuthenticationEvent} and the {@code idpEntityId} is
-	 * *not* null, and the
-	 * {@link ShibbolethIdpAuthenticationEvent#getServiceId()} of the event
-	 * being processed *is* null.
+	 * {@link ShibbolethIdpAuthenticationEvent} and the {@code idpEntityId} is *not*
+	 * null, and the {@link ShibbolethIdpAuthenticationEvent#getServiceId()} of the
+	 * event being processed *is* null.
 	 *
 	 * @param events
 	 *            the {@link List} of {@link Event}s to process.
 	 */
->>>>>>> 30b5806520f1ecbdd733222c390b9a2c310ce675
 	@Override
 	public void enrich(final List<Event> events) {
 		log.info(
@@ -82,13 +65,9 @@ public class IdpEntityIdEnricher implements AttributeEnrichment {
 
 			if (event instanceof ShibbolethIdpAuthenticationEvent) {
 
-<<<<<<< HEAD
 				if (((ShibbolethIdpAuthenticationEvent) event).getServiceId() == null && idpEntityId != null) {
 					log.trace("Adding idpEntityIdp [{}] as serviceId to eventID [{}]", idpEntityId, event.getEventId());
-=======
-				if ((((ShibbolethIdpAuthenticationEvent) event).getServiceId() == null) && (idpEntityId != null)) {
-					log.trace("Adding idpEntityIdp [{}] as serviceId to event [{}]", idpEntityId, event.getEventId());
->>>>>>> 30b5806520f1ecbdd733222c390b9a2c310ce675
+
 					((ShibbolethIdpAuthenticationEvent) event).setServiceId(idpEntityId);
 				}
 			}
